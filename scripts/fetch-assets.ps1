814c6b2394e870545df764d0303cd257a9c511a1
brian200508 <36002167+brian200508@users.noreply.github.com>
2026-07-21 18:22:27 +0200

    fix grimler repo

--- scripts/fetch-assets.ps1
+++ scripts/fetch-assets.ps1
@@ -25,7 +25,7 @@
 #>
 
 param(
-    [string]$prootDebUrl = "https://grimler.se/termux/termux-main/pool/main/p/proot/proot_5.1.107.84_aarch64.deb"
+    [string]$prootDebUrl = "https://grimler.se/termux-packages-24/pool/main/p/proot/proot_5.1.107.86_aarch64.deb"
 )
 
 $ErrorActionPreference = "Stop"
